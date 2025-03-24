b1cd78dc0ffb0d8df60dba3cfbf04bfb44cb70f7
varsharc <architv93@gmail.com>
2025-03-24 22:15:01 +0530

    Update _app.js to include Firebase context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css';
+import { FirebaseProvider } from '../contexts/FirebaseContext';
+import { ThemeProvider } from '../components/ThemeProvider';
+import Head from 'next/head';
 
+/**
+ * Custom Next.js App component that wraps the entire application
+ * Provides global context providers for Firebase and theme
+ */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <FirebaseProvider>
+      <ThemeProvider>
+        <Head>
+          <title>Intention-Ally | Semantic Search & Knowledge Visualization</title>
+          <meta name="description" content="Discover high-quality information with semantic search and interactive knowledge visualization." />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </FirebaseProvider>
+  );
 }
 
 export default MyApp;
